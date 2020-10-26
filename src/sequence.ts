5e333ceb5f23e51a1a722e9b98167c2166d8b35c
akronim <marinko.miskovic.hr@gmail.com>
2020-10-26 09:08:19 +0100

    First commit

---
+++ src/sequence.ts
@@ -0,0 +1,3 @@
+import {MiddlewareSequence} from '@loopback/rest';
+// used to define the set of actions to take during a REST request/response
+export class MySequence extends MiddlewareSequence {}
