4d7df06664366f202ac9633bcfa20873d7505e02
akronim <marinko.miskovic.hr@gmail.com>
2020-10-27 16:11:03 +0100

    Added todo-list model + todo-list repo

--- src/repositories/todo.repository.ts
+++ src/repositories/todo.repository.ts
@@ -1,16 +1,19 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Todo, TodoRelations} from '../models';
-import {DbDataSource} from '../datasources';
 import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {DbDataSource} from '../datasources';
+import {Todo, TodoRelations} from '../models';
 
 export class TodoRepository extends DefaultCrudRepository<
   Todo,
   typeof Todo.prototype.id,
   TodoRelations
 > {
-  constructor(
-    @inject('datasources.db') dataSource: DbDataSource,
-  ) {
+  constructor(@inject('datasources.db') dataSource: DbDataSource) {
     super(Todo, dataSource);
+  }
+
+  // Add the following function
+  public findByTitle(title: string) {
+    return this.findOne({where: {title}});
   }
 }
